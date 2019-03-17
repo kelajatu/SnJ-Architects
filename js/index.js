d1c4b5950314049246f569bf183d561af342f8de
your <jurgenkela@gmail.com>
2019-03-17 18:38:54 -0400

    menu nav click feature working

---
+++ js/index.js
@@ -0,0 +1,17 @@
+//  ========= Menu Nav =======
+
+const menuButton = document.querySelector('.open-hamburger'); 
+const menuOpenContent = document.querySelector('.menu-content'); 
+const closeMenuButton = document.querySelector('.closed-hamburger'); 
+
+menuButton.addEventListener('click', function() {
+    menuOpenContent.classList.remove('menu-hidden'); 
+    menuButton.style.display = 'none'; 
+    closeMenuButton.style.display = "flex"; 
+})
+
+closeMenuButton.addEventListener('click', function() {
+    menuOpenContent.classList.add('menu-hidden'); 
+    menuButton.style.display = "flex"; 
+    closeMenuButton.style.display = "none";
+})
